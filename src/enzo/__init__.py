dcdb826c9315d498e8ab8a4639a4c6ca940b08f7
Thomas Breydo <tbreydo@gmail.com>
2020-07-16 17:21:25 -0400

    add support for activation functions

--- src/enzo/__init__.py
+++ src/enzo/__init__.py
@@ -1,3 +1,4 @@
 from .neuron import Neuron
+from . import activation
 
 __version__ = '0.0.1.dev3'
--- src/enzo/neuron.py
+++ src/enzo/neuron.py
@@ -1,7 +1,13 @@
 import numpy as np
+from . import activation as act
 
 
 class Neuron:
-    def __init__(self, weights=None, bias=None):
+    def __init__(self, weights=None, bias=None, activation=None):
+        if activation is None:
+            activation = act.relu
         self.weights = weights
         self.bias = bias
+        self.activation = activation
+
+    # TODO: predict

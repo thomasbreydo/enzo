ff4b8cb19deb49642d3988ed4ffd3e85a11bd379
Thomas Breydo <tbreydo@gmail.com>
2020-07-16 17:53:19 -0400

    implement sigmoid activation

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -10,3 +10,17 @@ def test_positive_relu():
 def test_negative_relu():
     assert act.relu(-59) == 0
     assert act.relu(0) == 0
+
+
+def test_positive_sigmoid():
+    assert act.sigmoid(5) == pytest.approx(0.9933071490757153)
+    assert act.sigmoid(5000) == pytest.approx(1)
+
+
+def test_negative_sigmoid():
+    assert act.sigmoid(-9) == pytest.approx(0.00012339457598623172)
+    assert act.sigmoid(-5000) == pytest.approx(0)
+
+
+def test_zero_sigmoid():
+    assert act.sigmoid(0) == pytest.approx(0.5)

100b98e83da7a6f861bb0b3f6c3c5edfcfef1650
Thomas Breydo <tbreydo@gmail.com>
2020-07-17 16:55:38 -0400

    change versioning into RE, prep for docs

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+import re
+
+with open('src/enzo/__init__.py', encoding='utf8') as f:
+    version = re.search(R'__version__ = "(.*?)"', f.read()).group(1)
+
 
 # Metadata in setup.cfg
 setup(
     install_requires=[
         'numpy==1.19.0',
-    ]
+    ],
+    version=version,
 )

14868e8a6819f554cb005a85e3758cf0eaae76ef
Thomas Breydo <tbreydo@gmail.com>
2020-07-17 18:25:37 -0400

    test 2 (not 1) forward pass
    it's obviously important that outputs from one layer can proceed to the
    
    next

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -13,8 +13,10 @@ def test_dense_layer_init():
 
 
 def patched_randn(*args):
+    if args == (3, 1):
+        return [[0], [0], [-1]]  # weights for layer 2
     if args == (2, 1):
-        return [[0.3], [-0.1]]  # weights
+        return [[0.3], [-0.1]]  # weights for layer 1
     if args == ():
         return 1   # bias
     raise ValueError('unexpected args passed')
@@ -23,7 +25,12 @@ def patched_randn(*args):
 def test_dense_layer_forward(mocker):
     patched = mocker.patch('enzo.units.np.random.randn',
                            side_effect=patched_randn)
-    layer = enzo.layers.DenseLayer(3)
+    layer1 = enzo.layers.DenseLayer(3)
+    layer2 = enzo.layers.DenseLayer(2)
     inputs = [[1, 2], [-0.5, 0.1]]
-    assert layer.forward(inputs) == [[pytest.approx(1.1)] * 3,
-                                     [pytest.approx(0.84)] * 3]
+    after_layer1 = layer1.forward(inputs)
+    assert after_layer1 == [[pytest.approx(1.1)] * 3,
+                            [pytest.approx(0.84)] * 3]
+    after_layer2 = layer2.forward(after_layer1)
+    assert after_layer2 == [[pytest.approx(-0.1)] * 2,
+                            [pytest.approx(0.16)] * 2]

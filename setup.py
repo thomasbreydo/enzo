7d97c6038cc2e348ed4b11b052e4a67c75c06dcb
Thomas Breydo <tbreydo@gmail.com>
2020-08-08 15:51:52 -0400

    Fix version RE because Black prefers " over '

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 import re
 
-with open('src/enzo/__init__.py', encoding='utf8') as f:
-    version = re.search(R"__version__ = '(.*?)'", f.read()).group(1)
+with open("src/enzo/__init__.py", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 
 # Metadata in setup.cfg
 setup(
-    install_requires=[
-        'numpy==1.19.0',
-    ],
-    version=version,
+    install_requires=["numpy==1.19.0",], version=version,
 )

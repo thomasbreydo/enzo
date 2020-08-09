650eb451f8a54afd3e998217e07f470454e992ad
Thomas Breydo <tbreydo@gmail.com>
2020-08-08 21:36:54 -0400

    Convert weight storage to matrix format

--- src/enzo/layers.py
+++ src/enzo/layers.py
@@ -1,25 +1,46 @@
-from .units import Neuron
+import numpy as np
+from . import activations
 
 
 class DenseLayer:
-    '''TODO: add desc
+    """TODO: add desc, allow implicit input_length, finish docs
 
     Parameters
     ----------
+    input_length : int
+        The length of the vector of inputs this layer will receive. For hidden layers,
+        this is the number of units in the previous layer.
     n_units : int
-        The number of `class:Neuron` instances to add to this layer.
-    weights : list of numeric
-        The initial list of weights for all neurons.
-    bias : numeric
-        The initial bias for all neurons.
+        The number of neurons in the layer.
     activation : callable
         The activation function for this layer.
-    '''
 
-    def __init__(self, n_units, weights=None, bias=None, activation=None):
-        self.neurons = [Neuron(weights, bias, activation)
-                        for _ in range(n_units)]
+    Notes
+    -----
+    The weights matrix (``self.weights``) has each column corresponding to one unit's
+    weights. This allows forward propagation with a matrix where each row is one sample
+    to be ``__matmul__``-ed with ``self.weights`` to generate activations.
+    """
 
-    def forward(self, inputs):
-        return [[neuron.process(inp) for neuron in self.neurons]
-                for inp in inputs]
+    def __init__(self, input_length, n_units, activation=None):
+        if activation is None:
+            activation = activations.relu
+        self.weights = np.random.rand(input_length + 1, n_units)
+        self.input_length = input_length
+        self.n_units = n_units
+        self.activation = activation
+        self.outputs = None
+
+    @staticmethod
+    def _append_column_of_ones(samples):
+        """Prepare matrix of samples for multiplication with a matrix of weights.
+
+        This allows for the bias to be taken into account.
+        """
+        n_samples = len(samples)
+        return np.append(samples, np.ones(n_samples).reshape(n_samples, 1), axis=1)
+
+    def forward(self, samples):
+        pre_actiation_func = DenseLayer._append_column_of_ones(samples) @ self.weights
+        self.outputs = self.activation(pre_actiation_func)
+        return self.outputs

712d7467d3f8553b079d5dcb27a06b0fbdbef707
Thomas Breydo <tbreydo@gmail.com>
2020-08-09 17:40:39 -0400

    Ignore n as bad name + no docstring in setup.py
    For short activation functions that perform mathematical operations on
    one numerical input, it makes sense to allow "n" as a function argument.
    Docstring shouldn't be present in setup.py file.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# pylint: disable=C0114 (missing-module-docstring)
 import re
 from setuptools import setup
 
@@ -5,7 +6,4 @@ with open("src/enzo/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 
-# Metadata in setup.cfg
-setup(
-    install_requires=["numpy==1.19.0",], version=version,
-)
+setup(install_requires=["numpy==1.19.0",], version=version)

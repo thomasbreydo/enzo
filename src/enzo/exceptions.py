d8e8032ee60ab7c1b11f9cd4fec824e509e1be65
Thomas Breydo <tbreydo@gmail.com>
2020-08-12 17:57:10 -0400

    Fix typo

--- src/enzo/exceptions.py
+++ src/enzo/exceptions.py
@@ -5,8 +5,5 @@ class BackPropagationBeforeForwardException(Exception):
     """Raise when back-propagation is run before forward-propagation"""
 
 
-red
-
-
 class LayerBuildingError(Exception):
     """Raise when :func:`layers.Layer.build` fails."""
